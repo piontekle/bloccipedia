57242ffa146b2e5f270ea9de12b9b5c1b91fa1cd
Lauren Piontek <piontekle@gmail.com>
2019-03-26 19:56:11 -0400

    heroku secret fix

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -17,7 +17,7 @@ module.exports = {
     app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(expressValidator());
     app.use(session({
-      secret: process.env.cookieSecret,
+      secret: process.env.DATABASE_URL,
       resave: false,
       saveUninitialized: false,
       cookie: { maxAge: 1.21e+9 }
